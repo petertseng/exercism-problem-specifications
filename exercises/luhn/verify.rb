124cf3854d6223da4c47b218605db1a7a5852830
Peter Tseng <pht24@cornell.edu>
2017-01-15 12:14:25 +0000

    verify luhn

---
+++ exercises/luhn/verify.rb
@@ -0,0 +1,29 @@
+require 'json'
+require_relative '../../verify'
+
+json = JSON.parse(File.read(File.join(__dir__, 'canonical-data.json')))
+
+class LuhnError < StandardError; end
+
+def map_luhn_digit(digit, double)
+  begin
+    digit = Integer(digit)
+  rescue ArgumentError
+    raise LuhnError
+  end
+  double && digit != 9 ? digit * 2 % 9 : digit
+end
+
+def valid_luhn?(input, map_digit = method(:map_luhn_digit), modulus: 10)
+  return false if input.strip.size < 2
+
+  input.delete(' ').reverse.chars.zip([false, true].cycle).sum { |digit, double|
+    map_digit[digit, double]
+  } % modulus == 0
+rescue LuhnError
+  return false
+end
+
+verify(json['cases'], property: 'valid') { |i, _|
+  valid_luhn?(i['value'])
+}
