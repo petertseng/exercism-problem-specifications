38193417d11844f1208fb9ff3a5a32b1e887c537
Peter Tseng <pht24@cornell.edu>
2017-03-08 14:03:40 +0000

    verify prime-factors

---
+++ exercises/prime-factors/verify.rb
@@ -0,0 +1,9 @@
+require 'json'
+require 'prime'
+require_relative '../../verify'
+
+json = JSON.parse(File.read(File.join(__dir__, 'canonical-data.json')))
+
+verify(json['cases'].flat_map { |c| c['cases'] }, property: 'factors') { |i, _|
+  Prime.prime_division(i['value']).flat_map { |f, n| [f] * n }
+}
