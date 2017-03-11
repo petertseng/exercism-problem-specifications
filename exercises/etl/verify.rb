00c30a40e1759fec73a904809a76a34ff5e997f9
Peter Tseng <pht24@cornell.edu>
2017-03-11 20:22:58 +0000

    verify etl

---
+++ exercises/etl/verify.rb
@@ -0,0 +1,10 @@
+require 'json'
+require_relative '../../verify'
+
+json = JSON.parse(File.read(File.join(__dir__, 'canonical-data.json')))
+
+verify(json['cases'].flat_map { |c| c['cases'] }, property: 'transform') { |i, _|
+  i['legacy'].flat_map { |score, letters|
+    letters.map { |letter| [letter.downcase, score.to_i] }
+  }.to_h
+}
