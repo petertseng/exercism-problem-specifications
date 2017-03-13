19c563227778d617efb0338e9fc05e107533f051
Peter Tseng <pht24@cornell.edu>
2017-03-13 08:14:06 +0000

    verify trinary

---
+++ exercises/trinary/verify.rb
@@ -0,0 +1,9 @@
+require 'json'
+require_relative '../../verify'
+
+json = JSON.parse(File.read(File.join(__dir__, 'canonical-data.json')))
+
+# This is a ridiculous error condition, but the exercise is deprecated so who cares?
+verify(json['cases'], error: ->(c) { c['expected'] == 0 }, property: 'toDecimal') { |i, _|
+  Integer(i['trinary'].to_s, 3)
+}
