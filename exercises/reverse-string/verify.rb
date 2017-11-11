abaed20fee3daab835ac499fc22dc087352c93da
Peter Tseng <pht24@cornell.edu>
2017-11-11 08:53:49 +0000

    verify reverse-string

---
+++ exercises/reverse-string/verify.rb
@@ -0,0 +1,6 @@
+require 'json'
+require_relative '../../verify'
+
+json = JSON.parse(File.read(File.join(__dir__, 'canonical-data.json')))
+
+verify(json['cases'], property: 'reverse') { |i, _| i['value'].reverse }
