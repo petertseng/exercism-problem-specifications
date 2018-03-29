9c08ee952600154ee82dae91336ed9ce46f2999f
Peter Tseng <pht24@cornell.edu>
2018-03-29 11:42:59 +0000

    anti-verify change

--- exercises/change/verify.rb
+++ exercises/change/verify.rb
@@ -21,6 +21,70 @@ end
 
 json = JSON.parse(File.read(File.join(__dir__, 'canonical-data.json')))
 
-verify(json['cases'], property: 'findFewestCoins') { |i, _|
-  fewest_coins(i['target'], i['coins'])
-}
+multi_verify(json['cases'], property: 'findFewestCoins', implementations: [
+  {
+    name: 'correct',
+    f: -> (i, _) {
+      fewest_coins(i['target'], i['coins'])
+    },
+  },
+  {
+    name: 'greedy',
+    should_fail: true,
+    f: -> (i, c) {
+      remainder = i['target']
+      coins = []
+      i['coins'].sort.reverse_each { |coin|
+        while remainder >= coin
+          remainder -= coin
+          coins << coin
+        end
+      }
+
+      if coins.sum == i['target']
+        coins.sort
+      elsif c['expected'].is_a?(Array)
+        nil
+      else
+        raise 'fail as expected'
+      end
+    },
+  },
+  {
+    # https://github.com/exercism/problem-specifications/pull/882
+    name: 'greedy but careful',
+    should_fail: true,
+    f: -> (i, c) {
+      best_coins = [0] * (i['target'] + 1)
+      smallest_coin = i['coins'].min
+
+      # This solution chooses the smallest N coins,
+      # knowing that large coins may lead to wrong solutions.
+      # Of the coins selected, it starts from larger coins,
+      # but ensures the solution remains still possible.
+      #
+      # To defeat it, craft a case where:
+      # At the point where you must stop using some coin C,
+      # the remainer still exceeds C + the smallest coin.
+      (1..i['coins'].size).each { |n|
+        remainder = i['target']
+        my_coins = []
+        i['coins'].sort.first(n).reverse_each { |coin|
+          while remainder >= coin && (remainder - coin == 0 || remainder - coin >= smallest_coin)
+            remainder -= coin
+            my_coins << coin
+          end
+        }
+        best_coins = my_coins if remainder == 0 && best_coins.size > my_coins.size
+      }
+
+      if best_coins.sum == i['target']
+        best_coins.sort
+      elsif c['expected'].is_a?(Array)
+        nil
+      else
+        raise 'fail as expected'
+      end
+    },
+  },
+])
