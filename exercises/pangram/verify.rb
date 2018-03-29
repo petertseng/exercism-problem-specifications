f2fe20fcd17b8e2af7581d68d97579446e0e78f0
Peter Tseng <pht24@cornell.edu>
2018-03-29 11:38:27 +0000

    anti-verify pangram

--- exercises/pangram/verify.rb
+++ exercises/pangram/verify.rb
@@ -7,9 +7,44 @@ ALL_LETTERS = 0x7fffffe
 A = ?A.ord
 Z = ?z.ord
 
-verify(json['cases'], property: 'isPangram') { |i, _|
-  seen = 0
-  i['sentence'].each_codepoint.any? { |cp|
-    A <= cp && cp <= Z && (seen = (seen | 1 << (cp & 31)) & ALL_LETTERS) == ALL_LETTERS
-  }
-}
+multi_verify(json['cases'], property: 'isPangram', implementations: [
+  {
+    name: 'correct',
+    f: ->(i, _) {
+      seen = 0
+      i['sentence'].each_codepoint.any? { |cp|
+        A <= cp && cp <= Z && (seen = (seen | 1 << (cp & 31)) & ALL_LETTERS) == ALL_LETTERS
+      }
+    },
+  },
+  {
+    name: 'by hash',
+    f: ->(i, _) {
+      chars = i['sentence'].upcase.each_char.to_h { |k| [k, nil] }
+      (?A..?Z).all? { |l| chars.has_key?(l) }
+    },
+  },
+  {
+    name: 'just uniques',
+    should_fail: true,
+    f: ->(i, _) {
+      i['sentence'].chars.uniq.size == 26
+    },
+  },
+  # https://github.com/exercism/problem-specifications/pull/852
+  {
+    name: 'case-insensitive',
+    should_fail: true,
+    f: ->(i, _) {
+      i['sentence'].chars.uniq.count { |c| (?a..?z).cover?(c) || (?A..?Z).cover?(c) } == 26
+    },
+  },
+  # https://github.com/exercism/problem-specifications/pull/228
+  {
+    name: 'word characters',
+    should_fail: true,
+    f: ->(i, _) {
+      i['sentence'].gsub(/[^\w]/, '').chars.uniq.size == 26
+    },
+  },
+])
