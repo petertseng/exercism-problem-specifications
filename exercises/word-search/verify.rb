af0df79c0757ed8c14489726d44c344341d415b9
Peter Tseng <pht24@cornell.edu>
2018-03-30 03:00:16 +0000

    anti-verify word-search

--- exercises/word-search/verify.rb
+++ exercises/word-search/verify.rb
@@ -3,12 +3,12 @@ require_relative '../../verify'
 
 DIRS = [-1, 0, 1].product([-1, 0, 1]).tap { |x| x.delete([0, 0]) }.freeze
 
-def find_word(grid, word, dirs, starts)
+def find_word(grid, word, dirs, starts, off_by_one: false)
   # one-index.
   coord = ->(y, x) {{'row' => y + 1, 'column' => x + 1}}
 
   (starts || []).product(dirs).each { |(y, x), (dy, dx)|
-    next unless word.each_char.with_index.all? { |c, i|
+    next unless word.chars[0..(off_by_one ? -2 : -1)].each_with_index.all? { |c, i|
       cy = y + dy * i
       cx = x + dx * i
       next false if cy < 0 || cx < 0
@@ -24,15 +24,25 @@ def find_word(grid, word, dirs, starts)
   nil
 end
 
-def find_words(grid, words, dirs)
+def find_words(grid, words, dirs, **kwargs)
   starts = grid.flat_map.with_index { |row, y|
     (0...row.size).map { |x| [y, x] }
   }.group_by { |y, x| grid[y][x] }
-  words.to_h { |word| [word, find_word(grid, word, dirs, starts[word[0]])] }
+  words.to_h { |word| [word, find_word(grid, word, dirs, starts[word[0]], **kwargs)] }
 end
 
 json = JSON.parse(File.read(File.join(__dir__, 'canonical-data.json')))
 
-verify(json['cases'], property: 'search') { |i, _|
-  find_words(i['grid'], i['wordsToSearchFor'], DIRS)
-}
+dir_sets = [
+  ['all', DIRS],
+] + DIRS.map { |exclude| ["without #{exclude}", DIRS - [exclude]] }
+
+multi_verify(json['cases'], property: 'search', implementations: dir_sets.map { |name, ds| {
+  name: name,
+  should_fail: ds.size < DIRS.size,
+  f: ->(i, _) { find_words(i['grid'], i['wordsToSearchFor'], ds) },
+}} + [{
+  name: 'off-by-one',
+  should_fail: true,
+  f: ->(i, _) { find_words(i['grid'], i['wordsToSearchFor'], DIRS, off_by_one: true) },
+}])
