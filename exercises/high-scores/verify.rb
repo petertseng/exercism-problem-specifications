010fa65cbb3b93da6bce6d533c9ffd8a11536191
Peter Tseng <pht24@cornell.edu>
2018-10-27 00:20:04 +0000

    verify high-scores

---
+++ exercises/high-scores/verify.rb
@@ -0,0 +1,22 @@
+require 'json'
+require_relative '../../verify'
+
+json = JSON.parse(File.read(File.join(__dir__, 'canonical-data.json')))
+
+cases = by_property(json['cases'].flat_map { |c| c.has_key?('cases') ? c['cases'] : [c] }, %w(scores latest personalBest personalTopThree))
+
+verify(cases['scores'], property: 'scores') { |i, _|
+  i['scores']
+}
+
+verify(cases['latest'], property: 'latest') { |i, _|
+  i['scores'][-1]
+}
+
+verify(cases['personalBest'], property: 'personalBest') { |i, _|
+  i['scores'].max
+}
+
+verify(cases['personalTopThree'], property: 'personalTopThree') { |i, _|
+  i['scores'].max(3)
+}
