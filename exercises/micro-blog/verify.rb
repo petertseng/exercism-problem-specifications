1a058dedb90e118045ec57e4841531e150dcb5ef
Peter Tseng <pht24@cornell.edu>
2019-09-21 03:27:40 +0000

    verify/anti-verify micro-blog

---
+++ exercises/micro-blog/verify.rb
@@ -0,0 +1,20 @@
+require 'json'
+require_relative '../../verify'
+
+json = JSON.parse(File.read(File.join(__dir__, 'canonical-data.json')))
+
+multi_verify(json['cases'].flat_map { |c| c['cases'] }, property: 'truncate', implementations: [
+  {
+    name: 'string index',
+    f: ->(i, _) { i['phrase'][0...5] },
+  },
+  {
+    name: 'chars',
+    f: ->(i, _) { i['phrase'].chars.take(5).join },
+  },
+  {
+    name: 'bytes',
+    should_fail: true,
+    f: ->(i, _) { i['phrase'].bytes.take(5).pack('C*').force_encoding('utf-8') },
+  },
+])
